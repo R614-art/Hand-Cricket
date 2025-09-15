405b11eed4157c98da6f775335ae7c1b103c4e2c
charith vyayavaram <rohithvyayavaram.com>
2025-09-15 12:44:44 +0530

    changed cors

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const cors= require('cors');
 app.use(cors({
-  origin: "https://hand-cricket-rho.vercel.app/multioptions"
+  origin: "https://hand-cricket-rho.vercel.app"
 }));
 
 const io = new Server(server, {
